8f52846e53f0fd150ba883f2de8c09f33fc21162
venkat kamath <venky.kamath@gmail.com>
2020-06-28 19:23:35 +0530

    updates

--- src/main/java/com/venkat/LambdaMethodHandler.java
+++ src/main/java/com/venkat/LambdaMethodHandler.java
@@ -5,12 +5,13 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.LocalDateTime;
 
 public class LambdaMethodHandler {
     public void handleRequest(InputStream inputStream,
                                 OutputStream outputStream, Context context) throws Exception {
         String input = IOUtils.toString(inputStream, "UTF-8");
         outputStream.write(("Hello World - " + input).getBytes());
-        context.getLogger().log("log 2\n");
+        context.getLogger().log("log 2 " + LocalDateTime.now()+ "\n");
     }
 }
